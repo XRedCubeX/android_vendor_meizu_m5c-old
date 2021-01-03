311857f7f318195cea50d53b679f5798d7ea3494
XRed_CubeX <xredcubex@gmail.com>
2021-01-03 22:51:02 +0100

    first commit

---
+++ m5c-vendor.mk
@@ -0,0 +1,19 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/m5c/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/m5c/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/m5c/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/m5c/proprietary/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/m5c/proprietary/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/m5c/proprietary/vendor/bin,system/vendor/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/m5c/proprietary/vendor/etc,system/vendor/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/m5c/proprietary/vendor/firmware,system/vendor/firmware)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/m5c/proprietary/vendor/lib,system/vendor/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/m5c/proprietary/vendor/lib64,system/vendor/lib64) 
